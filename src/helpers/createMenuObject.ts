53c77bc961c6ab84c4eddb8df07ac765bfd9626b
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2024-09-13 16:19:46 -0300

    feat: helpers, models and views

---
+++ src/helpers/createMenuObject.ts
@@ -0,0 +1,16 @@
+type MenuObject = "" | "all" | "dog" | "cat" | "fish";
+
+export const createMenuObject = (activeMenu: MenuObject) => {
+    let returnObject = {
+        all: false,
+        dog: false,
+        cat: false,
+        fish: false
+    };
+
+    if (activeMenu !== "") {
+        returnObject[activeMenu] = true;
+    }
+
+    return returnObject;
+}
