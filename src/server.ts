c1dad77442c17924e9bf898f1e7d67fa5d0443e4
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2024-09-13 16:38:14 -0300

    feat: finished project - controllers, not found page and views

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ server.use(express.static(path.join(__dirname, "../public")));
 
 server.use(mainRoutes);
 server.use((req, res) => {
-    res.send("Not found page");
+    res.render("pages/404");
 });
 
 server.listen(process.env.PORT);
